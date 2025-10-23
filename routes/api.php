18d41862dddb1728b4597f30a867d9016d39c893
Bakary <bakarydiassy28@gmail.com>
2025-10-23 21:35:43 +0000

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok', 'message' => 'Application is healthy']);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
